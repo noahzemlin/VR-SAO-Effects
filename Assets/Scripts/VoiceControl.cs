c9f3c29674f8fed22e8bc6059788d86d4afde4dc
Justin Wilford <jwilford@usc.edu>
2019-05-12 22:19:48 -0700

    Added simple voice commands for 'Sword Skill', 'Use Item', and 'Communication Skill'

---
+++ Assets/Scripts/VoiceControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceControl : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actions.Add("sword skill", SwordSkill);
+        actions.Add("use item", UseItem);
+        actions.Add("communication skill", CommunicationSkill);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += ActivateSkill;
+        keywordRecognizer.Start();
+    }
+
+    private void ActivateSkill(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log("Found voice command: " + speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void SwordSkill()
+    {
+
+    }
+
+    private void UseItem()
+    {
+
+    }
+
+    private void CommunicationSkill()
+    {
+
+    }
+}
